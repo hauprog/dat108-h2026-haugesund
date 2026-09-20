df1b4626b31bb3f3a59e231cbf760331c92f0b36
Tord Waage Frøland <tfroland@gmail.com>
2026-09-20 20:37:30 +0200

    F10: LockedIn i nettleseren, web/index.html og web/spill.js

---
+++ LockedIn/web/spill.js
@@ -0,0 +1,16 @@
+console.log("De er i: Operasjonsstue 7");
+
+const tilstand = {
+    helt: { navn: "Ole", hp: 100, styrke: 5},
+    angripbare: [
+        { navn: "Rottekonge", hp: 20 },
+        { navn: "Rottedronning", hp: 500 },
+        { navn: "Rottebarn", hp: 9999 }
+    ],
+    skatter: [
+        { navn: "gull som brenner", verdi: 250 },
+        { navn: "sandaler", verdi: 250 },
+        { navn: "ostekake", verdi: 2147483647 },
+        { navn: "gulrotkake", verdi: 2147483646 }
+    ]
+}
