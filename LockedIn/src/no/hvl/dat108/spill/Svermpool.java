1dbf7fc5ec27a7399a9f9b204f4dc785c107e63b
Tord Waage Frøland <tfroland@gmail.com>
2026-09-20 20:36:58 +0200

    Svermpool - samle inn med get

--- LockedIn/src/no/hvl/dat108/spill/Svermpool.java
+++ LockedIn/src/no/hvl/dat108/spill/Svermpool.java
@@ -2,15 +2,12 @@ package no.hvl.dat108.spill;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
+import java.util.concurrent.*;
 import java.util.stream.IntStream;
 
 public class Svermpool {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         Callable<Integer> rotte = () -> {
             int mine = 0;
@@ -25,6 +22,18 @@ public class Svermpool {
         List<Future<Integer>> kvitteringer = IntStream.range(0, 10)
                 .mapToObj(i -> pool.submit(rotte))
                 .toList();
+
+        // Her er imperativ programmering kanskje å foretrekke for lesbarhetens skyld.
+        List<Future<Integer>> kvitteringer2 = new ArrayList<>();
+        for (int i = 0; i < 10; i++) kvitteringer2.add(pool.submit(rotte));
+
         // fredag: samle inn, planlagt feil, AtomicInteger, shutdown
+        // Samle inn: get venter hvis svaret ikke er klart
+        int total = 0;
+        for (Future<Integer> f : kvitteringer)
+            total += f.get();
+
+        pool.shutdown();
+        System.out.println("total: " + total + " (forventet 1_000_000.");
     }
 }
